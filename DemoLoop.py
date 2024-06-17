ff8ed074888ceb3e5aeabbc2ac7fc9ab889471f8
papasmf1 <papasmf1@gmail.com>
2024-06-17 16:50:03 +0900

    월요일 수업 진짜 완료

---
+++ DemoLoop.py
@@ -0,0 +1,16 @@
+# DemoLoop.py 
+
+value = 5 
+while value > 0:
+    print(value)
+    value -= 1 
+
+lst = ["python", 100, 3.14]
+for item in lst:
+    print(item)
+
+fruits = {"apple":100, "kiwi":200}
+for item in fruits.items():
+    print(item)
+
+    
