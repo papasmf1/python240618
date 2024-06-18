0a4a5869b678bda9cfc6f3d81ce448c4e69a0d51
papasmf1 <papasmf1@gmail.com>
2024-06-18 10:39:40 +0900

    클래스 연습 완료

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,13 +1,17 @@
+#전역변수 
 strName = "Not Class Member"
 
 class DemoString:
     def __init__(self):
+        #멤버변수
         self.strName = "" 
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(str)
+        #파이썬은 모호한 것보다 명시적인 것이 좋다! 
+        print(self.strName)
 
+#인스턴스 생성 
 d = DemoString()
 d.set("First Message")
 d.print()
--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,30 @@
+#부모클래스: 공통 코드를 준비 
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
 
+#자식클래스: 보다 구체적으로 구현
 class Student(Person):
+    #덮어쓰기(override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #부모 초기화 메서드 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #덮어쓰기
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))     
+        print("Info(학과:{0}, 학번: {1})".format(
+            self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+p.printInfo()
+s.printInfo() 
 
 
